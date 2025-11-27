be30975d54b15bdba338c495f259042fb344b1d3
Birsan Ionut <birsan.ionut210392@gmail.com>
2025-11-27 22:59:10 +0200

    ch5ex4

--- ch5/sq-ch5-ex1/src/main/java/example/CommentService.java
+++ ch5/sq-ch5-ex1/src/main/java/example/CommentService.java
@@ -1,9 +1,11 @@
 package example;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
 @Service
+@Lazy
 public class CommentService {
 
     public CommentService() {
