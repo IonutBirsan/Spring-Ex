e158450ab7e06e6406898f0f21116c4bcbbfb180
Birsan Ionut <birsan.ionut210392@gmail.com>
2025-11-27 23:10:39 +0200

    ch5ex5

--- ch5/sq-ch5-ex1/src/main/java/example/CommentService.java
+++ ch5/sq-ch5-ex1/src/main/java/example/CommentService.java
@@ -4,11 +4,5 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
-@Service
-@Lazy
 public class CommentService {
-
-    public CommentService() {
-        System.out.println("CommentService instance created!");
-    }
 }
--- ch5/sq-ch5-ex1/src/main/java/example/ProjectConfig.java
+++ ch5/sq-ch5-ex1/src/main/java/example/ProjectConfig.java
@@ -1,11 +1,17 @@
 package example;
 
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
-@ComponentScan(basePackages = {"example"})
 public class ProjectConfig {
 
+    @Bean
+    @Scope(BeanDefinition.SCOPE_PROTOTYPE)
+    public CommentService commentService() {
+        return new CommentService();
+    }
 }
