6b0ad07d018e775052f6ca01df0d201399491c5b
Birsan Ionut <birsan.ionut210392@gmail.com>
2025-11-28 15:57:18 +0200

    ch6ex1

---
+++ ch5/sq-ch5-ex1/src/main/java/example/CommentProcessor.java
@@ -0,0 +1,16 @@
+package example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope(BeanDefinition.SCOPE_PROTOTYPE)
+public class CommentProcessor {
+
+    @Autowired
+    private CommentRepository commentRepository;
+
+    // Omitted code
+}
---
+++ ch6/sq-ch6-ex1/src/main/java/example/Main.java
@@ -0,0 +1,18 @@
+package example;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+
+    public static void main(String[] args) {
+        var c = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        var service = c.getBean(CommentService.class);
+
+        Comment comment = new Comment();
+        comment.setText("Demo comment");
+        comment.setAuthor("Natasha");
+
+        service.publishComment(comment);
+    }
+}
