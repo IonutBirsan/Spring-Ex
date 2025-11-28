686b8459c8055565d33f58d2493e4c30ec073b5d
Birsan Ionut <birsan.ionut210392@gmail.com>
2025-11-28 19:44:07 +0200

    ch6ex5

--- ch6/sq-ch6-ex1/src/main/java/example/LoggingAspect.java
+++ ch6/sq-ch6-ex1/src/main/java/example/LoggingAspect.java
@@ -1,6 +1,7 @@
 package example;
 
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
@@ -12,27 +13,10 @@ import java.util.logging.Logger;
 @Component
 public class LoggingAspect {
 
-    private Logger logger =
-            Logger.getLogger(LoggingAspect.class.getName());
+    private Logger logger = Logger.getLogger(LoggingAspect.class.getName());
 
-    @Around("@annotation(ToLog)")
-    public Object log(ProceedingJoinPoint joinPoint) throws Throwable {
-        String methodName = joinPoint.getSignature().getName();
-        Object [] arguments = joinPoint.getArgs();
-
-
-        logger.info("Method " + methodName +
-                " with parameters " + Arrays.asList(arguments) +
-                " will execute");
-
-        Comment comment = new Comment();
-        comment.setText("Some other text!");
-        Object [] newArguments = {comment};
-
-        Object returnedByMethod = joinPoint.proceed(newArguments);
-
-        logger.info("Method executed and returned " + returnedByMethod);
-
-        return "FAILED";
+    @AfterReturning(value = "@annotation(ToLog)", returning = "returnedValue")
+    public void log(Object returnedValue) {
+        logger.info("Method executed and returned " + returnedValue);
     }
 }
