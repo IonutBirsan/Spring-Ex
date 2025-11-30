8f768ccd3d52016d762d16bafc3bf341f3c786f6
Birsan Ionut <birsan.ionut210392@gmail.com>
2025-11-30 12:48:34 +0200

    ch10ex3

---
+++ ch10/demo/src/main/java/example/controllers/CountryController.java
@@ -0,0 +1,15 @@
+package example.controllers;
+
+import example.model.Country;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CountryController {
+
+    @GetMapping("/france")
+    public Country france() {
+        Country c = Country.of("France", 67);
+        return c;
+    }
+}
