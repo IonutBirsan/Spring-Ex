f9e4c25d8e45cf2d40d32a34fc30ccb5284ddba1
Birsan Ionut <birsan.ionut210392@gmail.com>
2025-11-30 15:02:23 +0200

    ch10ex7

--- ch10/demo/src/main/java/example/controllers/PaymentController.java
+++ ch10/demo/src/main/java/example/controllers/PaymentController.java
@@ -7,31 +7,25 @@ import example.PaymentService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.logging.Logger;
 
 @RestController
 public class PaymentController {
 
-    private final PaymentService paymentService;
-
-    public PaymentController(PaymentService paymentService) {
-        this.paymentService = paymentService;
-    }
+    private static Logger logger =
+            Logger.getLogger(PaymentController.class.getName());
 
     @PostMapping("/payment")
-    public ResponseEntity<?> makePayment() {
-        try {
-            PaymentDetails paymentDetails =
-            paymentService.processPayment();
-            return ResponseEntity
+    public ResponseEntity<PaymentDetails> makePayment(
+            @RequestBody PaymentDetails paymentDetails) {
+
+        logger.info("Received payment " +
+                paymentDetails.getAmount());
+
+        return ResponseEntity
               .status(HttpStatus.ACCEPTED)
-                    .body(paymentDetails);
-        } catch (NotEnoughMoneyException e) {
-            ErrorDetails errorDetails = new ErrorDetails();
-            errorDetails.setMessage("Not enough money to make the payment.");
-            return ResponseEntity
-              .badRequest()
-                    .body(errorDetails);
-        }
-    }
-}
+                .body(paymentDetails);
+    }}
