24124728ff45999e551ecfc30d3f86dc6fc6cd1a
Birsan Ionut <birsan.ionut210392@gmail.com>
2025-11-30 12:54:29 +0200

    ch10ex4 again

--- ch10/demo/src/main/java/example/controllers/CountryController.java
+++ ch10/demo/src/main/java/example/controllers/CountryController.java
@@ -1,6 +1,8 @@
 package example.controllers;
 
 import example.model.Country;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,14 +11,14 @@ import java.util.List;
 @RestController
 public class CountryController {
 
-    // Omitted code
-
-    @GetMapping("/all")
-    public List<Country> countries() {
-        Country c1 = Country.of("France", 67);
-        Country c2 = Country.of("Spain", 47);
-
-        return List.of(c1,c2);    
+    @GetMapping("/france")
+    public ResponseEntity<Country> france() {
+        Country c = Country.of("France", 67);
+        return ResponseEntity
+                .status(HttpStatus.ACCEPTED)
+            .header("continent", "Europe")
+            .header("capital", "Paris")
+            .header("favorite_food", "cheese and wine")
+            .body(c);                                      
     }
-
 }
