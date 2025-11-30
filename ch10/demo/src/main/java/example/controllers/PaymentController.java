cf4292bbd9072a45296753782fcd891968e3ae5a
Birsan Ionut <birsan.ionut210392@gmail.com>
2025-11-30 15:03:24 +0200

    ch10ex6

--- ch10/demo/src/main/java/example/controllers/PaymentController.java
+++ ch10/demo/src/main/java/example/controllers/PaymentController.java
@@ -7,25 +7,22 @@ import example.PaymentService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.logging.Logger;
 
 @RestController
 public class PaymentController {
 
-    private static Logger logger =
-            Logger.getLogger(PaymentController.class.getName());
+    private final PaymentService paymentService;
+
+    public PaymentController(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
 
     @PostMapping("/payment")
-    public ResponseEntity<PaymentDetails> makePayment(
-            @RequestBody PaymentDetails paymentDetails) {
-
-        logger.info("Received payment " +
-                paymentDetails.getAmount());
-
+    public ResponseEntity<PaymentDetails> makePayment() {
+        PaymentDetails paymentDetails = paymentService.processPayment();
         return ResponseEntity
-              .status(HttpStatus.ACCEPTED)
+                .status(HttpStatus.ACCEPTED)
                 .body(paymentDetails);
-    }}
+    }
+}
