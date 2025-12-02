7bfa19873ab8313fb704deaedbdae28ff5d5e1eb
Birsan Ionut <birsan.ionut210392@gmail.com>
2025-12-02 17:02:30 +0200

    ch11payments

---
+++ ch11/payment/src/main/java/example/SqCh11Payments.java
@@ -0,0 +1,13 @@
+package example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqCh11Payments {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqCh11Payments.class, args);
+	}
+
+}
