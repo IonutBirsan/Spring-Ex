5a43d8cd103c8ef857041e74d90aeb99b2d2704a
Birsan Ionut <birsan.ionut210392@gmail.com>
2025-12-04 14:23:14 +0200

    ch13ex1

--- ch12/demo/src/main/java/example/ProjectConfig.java
+++ ch12/demo/src/main/java/example/ProjectConfig.java
@@ -1,34 +1,10 @@
 package example;
 
-import com.zaxxer.hikari.HikariDataSource;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-import javax.sql.DataSource;
-
 @Configuration
+@ComponentScan
 public class ProjectConfig {
 
-    @Value("jdbc:postgresql://localhost:5432/spring_quickly")
-    private String datasourceUrl;
-
-    @Value("postgres")
-    private String datasourceUsername;
-
-    @Value("postgres")
-    private String datasourcePassword;
-
-    @Bean
-    public DataSource dataSource() {
-        HikariDataSource dataSource =
-        new HikariDataSource();
-
-        dataSource.setJdbcUrl(datasourceUrl);
-        dataSource.setUsername(datasourceUsername);
-        dataSource.setPassword(datasourcePassword);
-        dataSource.setConnectionTimeout(1000);
-
-        return dataSource;
-    }
 }
