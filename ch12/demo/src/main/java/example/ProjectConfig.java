ebbfc9758ae0e0e6a4eaf23599ee0e836817f993
Birsan Ionut <birsan.ionut210392@gmail.com>
2025-12-04 13:26:50 +0200

    ch11ex2 with postrgres

--- ch12/demo/src/main/java/example/ProjectConfig.java
+++ ch12/demo/src/main/java/example/ProjectConfig.java
@@ -1,9 +1,6 @@
 package example;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class ProjectConfig {
--- ch12/demo/src/main/java/example/PurchaseRepository.java
+++ ch12/demo/src/main/java/example/PurchaseRepository.java
@@ -16,8 +16,8 @@ public class PurchaseRepository {
     }
 
     public void storePurchase(Purchase purchase) {
-        String sql =
-        "INSERT INTO purchase VALUES (NULL, ?, ?)";
+
+        String sql = "INSERT INTO purchase (product, price) VALUES (?, ?)";
 
         jdbc.update(sql,
                 purchase.getProduct(),
