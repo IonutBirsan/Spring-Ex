b2f2fca90e7de0db5e224c0d9680f11ea488f30a
Birsan Ionut <birsan.ionut210392@gmail.com>
2025-12-04 10:11:35 +0200

    ch11ex1

--- ch11/demo/src/main/java/example/SqCh11Ex1Application.java
+++ ch11/demo/src/main/java/example/SqCh11Ex1Application.java
@@ -1,13 +1,14 @@
 package example;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SqCh11Payments {
+public class SqCh11Ex1Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SqCh11Payments.class, args);
+		SpringApplication.run(SqCh11Ex1Application.class, args);
 	}
 
 }
