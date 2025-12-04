f8ce32b3de2f2d591d486160d8867514eef5c258
Birsan Ionut <birsan.ionut210392@gmail.com>
2025-12-04 11:06:51 +0200

    ch11ex3

--- ch11/demo/src/main/java/example/ProjectConfig.java
+++ ch11/demo/src/main/java/example/ProjectConfig.java
@@ -3,12 +3,15 @@ package example;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
-public class AppConfig {
+public class ProjectConfig {
 
     @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
+    public WebClient webClient() {
+        return WebClient
+                .builder()
+            .build();
     }
 }
--- ch11/demo/src/main/java/example/controllers/PaymentsController.java
+++ ch11/demo/src/main/java/example/controllers/PaymentsController.java
@@ -5,6 +5,7 @@ import example.model.Payment;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 import java.util.UUID;
 
@@ -18,9 +19,10 @@ public class PaymentsController {
     }
 
     @PostMapping("/payment")
-    public Payment createPayment(
+    public Mono<Payment> createPayment(
             @RequestBody Payment payment
     ) {
-        return paymentsProxy.createPayment(payment);
+        String requestId = UUID.randomUUID().toString();
+        return paymentsProxy.createPayment(requestId, payment);
     }
 }
